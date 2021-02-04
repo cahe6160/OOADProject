4c6a8541551d7913c460369418c3cfce5990558a
Gabriel Madera <gama9813@colorado.edu>
2021-02-03 18:50:34 -0700

    Pair 1 Comment added

--- src/edu/colorado/mtoftheholycross/Ship.java
+++ src/edu/colorado/mtoftheholycross/Ship.java
@@ -7,6 +7,8 @@ public class Ship {
     // TODO: Understand encapsulation
     // TODO: Understand what these todo comments mean
 
+    //Team mtoftheholycross, pair 1 was here
+
     public  void show()     {     // dunno why this is here maybe it is just an example method
         System.out.println("IF you can't see this then something is severely wrong!!");
     }
