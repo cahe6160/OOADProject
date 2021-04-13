d335a1ede6393ca894b4a34d50f0c99b5993296b
Gabriel Madera <gama9813@colorado.edu>
2021-04-12 20:13:24 -0600

    Finish Milestone6 Refactoring

--- src/edu/colorado/mtoftheholycross/Laser.java
+++ src/edu/colorado/mtoftheholycross/Laser.java
@@ -1,33 +1,22 @@
 package edu.colorado.mtoftheholycross;
 
-public class Laser extends Weapon {
+public class Laser extends Cannon {
     public void makeHit(String location, Grid opponentBoard){
+
+        super.makeHit(location, opponentBoard);
 
         int[] position = opponentBoard.convertPosition(location);
         int hitRow = position[0];
         int hitCol = position[1];
-
-        this.emptyHit();
-
-        if(opponentBoard.getMyShips()[hitRow][hitCol].getSurface().equals("Ship")) {
-            this.setShipHit(true);
-        } else if(opponentBoard.getMyShips()[hitRow][hitCol].getSurface().equals("Captain")){
-            this.setShipHit(true);
-            this.setCaptainHit(true);
-        } else if(opponentBoard.getMyShips()[hitRow][hitCol].getSurface().equals("Armor")){
-            this.setShipHit(true);
-            this.setArmorHit(true);
-        }
 
         if(opponentBoard.getMyShips()[hitRow][hitCol].getUnderwater().equals("Ship")) {
             this.setUnderShipHit(true);
         } else if(opponentBoard.getMyShips()[hitRow][hitCol].getUnderwater().equals("Captain")){
             this.setUnderShipHit(true);
             this.setUnderCaptainHit(true);
-        } else if(opponentBoard.getMyShips()[hitRow][hitCol].getUnderwater().equals("Armor")){
+        } else if(opponentBoard.getMyShips()[hitRow][hitCol].getUnderwater().equals("Armor")) {
             this.setUnderShipHit(true);
             this.setUnderArmorHit(true);
         }
-
     }
 }
