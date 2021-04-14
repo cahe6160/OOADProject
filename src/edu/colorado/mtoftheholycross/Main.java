e47420a0e496ec68d9b4602906092e7212a7d74d
Gabriel Madera <gama9813@colorado.edu>
2021-04-14 17:54:19 -0600

    Refactor Project Tree

--- src/edu/colorado/mtoftheholycross/Main.java
+++ src/edu/colorado/mtoftheholycross/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // ------------------------
         /*Game gameTest = new Game();
 
         gameTest.p1Grid.addShip(gameTest.getP1Fleet()[0]);
